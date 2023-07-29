d3673ad5beacf4ea137e3e47a4b717184d1d9c0d
Mark Reid <contact@markreid.com.au>
2023-07-29 17:13:27 +1000

    Show current/total periods in game clock

--- src/Timer.js
+++ src/Timer.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { leadingZero, formatClock } from './util';
+import "./Timer.css";
+import { formatClock } from "./util";
 
-import './Timer.css';
-
-
-
-
-const Timer = ({ clockTime, periodLength, toggleClock, resetClock, clockRunning }) => {
-
+const Timer = ({
+	clockTime,
+	periodLength,
+	toggleClock,
+	resetClock,
+	clockRunning,
+	currentPeriod,
+	numPeriods,
+}) => {
 	// toggle between display modes
 	// either show the timer going up, or the time remaining
 	const [remainingMode, setRemainingMode] = useState(true);
 	const toggleMode = () => setRemainingMode((mode) => !mode);
 
 	// hide the buttons by default so you don't push them by mistake
-	const [showButtons, setShowButtons] = useState(true);
+	const [showButtons, setShowButtons] = useState(false);
 
 	return (
 		<div className="timer">
-			<button onClick={toggleMode} className="timer-face">
-				{formatClock(remainingMode ? periodLength - clockTime : clockTime)}
+			<div className="timer-face">
+			<button onClick={toggleMode} className="timer-face-clock">
+				{formatClock(
+					remainingMode ? periodLength - clockTime : clockTime
+				)}				
 			</button>
-			
+			<p className="timer-face-periods">{currentPeriod} / {numPeriods}</p>
+			</div>
+
+
 			{showButtons ? (
 				<div className="timer-buttons">
-					<button className="timer-button" onClick={toggleClock}>{clockRunning ? '⏸' : '▶️' }</button>
-      				<button className="timer-button" onClick={resetClock}>🔁</button>
-      			</div>) 
-			: null}			
+					<button className="timer-button" onClick={toggleClock}>
+						{clockRunning ? "⏸" : "▶️"}
+					</button>
+					<button className="timer-button" onClick={resetClock}>
+						🔁
+					</button>
+				</div>
+			) : null}
 		</div>
 	);
 };
